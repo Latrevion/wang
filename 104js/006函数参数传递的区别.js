10a5615717fd0e693279ddb1115e8413a4978178
Latrevion <xiaopikaqiu@outlook.com>
2022-08-08 12:05:02 +0800

    数组与函数

---
+++ 104js/006函数参数传递的区别.js
@@ -0,0 +1,48 @@
+// // 原始类型的参数
+// var a = 5
+// var b= 3
+// function exchange(x,y) {
+//   var tmp = x;
+//   x =y
+//   y =tmp
+// }
+// exchange(a, b)
+// console.log(a, b)
+//
+//
+// ///////////
+// st001 a 5
+// st002 b 3
+// st003
+// st004
+//
+// //////////
+// hp001
+// hp002
+// hp003
+// hp004
+//
+//
+//
+// // 引用参数类型
+// var a =[5]
+// var b=[3]
+// function exchange(x,y) {
+//   var tmp = x[0]
+//   x[0]=y[0]
+//   y[0]=tmp
+// }
+// exchange(a, b)
+// console.log(a, b)
+//
+// ///////////
+// st001
+// st002
+// st003
+// st004
+//
+// //////////
+// hp001
+// hp002
+// hp003
+// hp004
